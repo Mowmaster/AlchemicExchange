3c352ed58bf6f281011ef03fd658e25e9738ef6c
Mowmaster <mowmaster@outlook.com>
2018-01-31 18:00:28 -0600

    Final commit for ModOff

--- src/main/java/com/mowmaster/alchex/references/Reference.java
+++ src/main/java/com/mowmaster/alchex/references/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MODID = "alchex";
     public static final String MODNAME = "Alchemic Exchange";
-    public static final String VERSION = "0.000.1";
+    public static final String VERSION = "0.000.2";
 
     public static final String CLIENT_SIDE = "com.mowmaster.alchex.proxies.ClientProxy";
     public static final String SERVER_SIDE = "com.mowmaster.alchex.proxies.CommonProxy";
